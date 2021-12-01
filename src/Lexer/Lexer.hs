afc4bcc472bb1a1bca1362fc1cbbd492e0fab063
Russel Arbore <russel.jma@gmail.com>
2021-12-01 09:55:49 -0600

    More lexer refactoring

--- src/Lexer/Lexer.hs
+++ src/Lexer/Lexer.hs
@@ -26,39 +26,39 @@ lexer :: Int -> Int -> T.Text -> [Token]
 lexer c l text -- (t:tx)
   | T.null text = []
   | p "\n" = lexer 0 (l + 1) $ T.tail text
-  | isSpace $ T.head text = lexHelper 1
-  | p "++" = (Token PlusPlus c l):(lexHelper 2)
-  | p "+=" = (Token PlusEquals c l):(lexHelper 2)
-  | p "+" = (Token Plus c l):(lexHelper 1)
-  | p "--" = (Token MinusMinus c l):(lexHelper 2)
-  | p "-=" = (Token MinusEquals c l):(lexHelper 2)
-  | p "-" = (Token Minus c l):(lexHelper 1)
-  | p "*=" = (Token StarEquals c l):(lexHelper 2)
-  | p "*" = (Token Star c l):(lexHelper 1)
-  | p "/=" = (Token SlashEquals c l):(lexHelper 2)
-  | p "/" = (Token Slash c l):(lexHelper 1)
-  | p "%=" = (Token PercentEquals c l):(lexHelper 2)
-  | p "%" = (Token Percent c l):(lexHelper 1)
-  | p "!=" = (Token ExclaEquals c l):(lexHelper 2)
-  | p "!" = (Token Excla c l):(lexHelper 1)
-  | p "^^" = (Token HatHat c l):(lexHelper 2)
-  | p "^=" = (Token HatEquals c l):(lexHelper 2)
-  | p "^" = (Token Hat c l):(lexHelper 1)
-  | p "||" = (Token BarBar c l):(lexHelper 2)
-  | p "|=" = (Token BarEquals c l):(lexHelper 2)
-  | p "|" = (Token Bar c l):(lexHelper 1)
-  | p "&&" = (Token AndAnd c l):(lexHelper 2)
-  | p "&=" = (Token AndEquals c l):(lexHelper 2)
-  | p "&" = (Token And c l):(lexHelper 1)
-  | p ">>=" = (Token RShiftEquals c l):(lexHelper 3)
-  | p ">>" = (Token RShift c l):(lexHelper 2)
-  | p ">=" = (Token GreaterEquals c l):(lexHelper 2)
-  | p ">" = (Token Greater c l):(lexHelper 1)
-  | p "<<=" = (Token LShiftEquals c l):(lexHelper 3)
-  | p "<<" = (Token LShift c l):(lexHelper 2)
-  | p "<=" = (Token LesserEquals c l):(lexHelper 2)
-  | p "<" = (Token Lesser c l):(lexHelper 1)
-  | p "==" = (Token EqualsEquals c l):(lexHelper 2)
-  | p "=" = (Token Equals c l):(lexHelper 1)
+  | isSpace $ T.head text = lexer (c + 1) l $ T.tail text
+  | p "++" = th PlusPlus 2
+  | p "+=" = th PlusEquals 2
+  | p "+" = th Plus 1
+  | p "--" = th MinusMinus 2
+  | p "-=" = th MinusEquals 2
+  | p "-" = th Minus 1
+  | p "*=" = th StarEquals 2
+  | p "*" = th Star 1
+  | p "/=" = th SlashEquals 2
+  | p "/" = th Slash 1
+  | p "%=" = th PercentEquals 2
+  | p "%" = th Percent 1
+  | p "!=" = th ExclaEquals 2
+  | p "!" = th Excla 1
+  | p "^^" = th HatHat 2
+  | p "^=" = th HatEquals 2
+  | p "^" = th Hat 1
+  | p "||" = th BarBar 2
+  | p "|=" = th BarEquals 2
+  | p "|" = th Bar 1
+  | p "&&" = th AndAnd 2
+  | p "&=" = th AndEquals 2
+  | p "&" = th And 1
+  | p ">>=" = th RShiftEquals 3
+  | p ">>" = th RShift 2
+  | p ">=" = th GreaterEquals 2
+  | p ">" = th Greater 1
+  | p "<<=" = th LShiftEquals 3
+  | p "<<" = th LShift 2
+  | p "<=" = th LesserEquals 2
+  | p "<" = th Lesser 1
+  | p "==" = th EqualsEquals 2
+  | p "=" = th Equals 1
     where p s = T.isPrefixOf (T.pack s) text
-          lexHelper n = lexer (c + n) l $ T.drop n text
+          th t n = (Token t c l):(lexer (c + n) l $ T.drop n text)
