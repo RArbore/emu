50458d4c59d48d5472b08fce55ce4b780e34c853
Russel Arbore <russel.jma@gmail.com>
2021-12-12 09:58:11 -0600

    Pretty print semantic errors

--- src/Semantics/Error.hs
+++ src/Semantics/Error.hs
@@ -18,22 +18,69 @@ module Semantics.Error
     ) where
 
 import Data.Text (Text)
+import qualified Data.Text as T
 
 import Parser.AST
      
-data SemanticsError = SemanticsError { offendingLine :: Text,
+data SemanticsError = SemanticsError { filename :: Text,
+                                       offendingLine :: Text,
                                        line :: Int,
                                        startCol :: Int,
                                        endCol :: Int,
                                        errorType :: SemanticsErrorType }
 data SemanticsErrorType = DuplicateDeclaration Text 
                         | VoidVarDeclaration Text
-                        | UndefinedSymbol Text
+                        | UndefinedIdentifier Text
                         | TypeError Type Type
                         | CastError Type Type
                         | CallError Text Int Int
                         | AddressError
                         | AssignError
                         | ImproperIdentifier Text
-                        | AccessError Text (Maybe Text)
+                        | FieldAccessError Text Text
+                        | LValueAccessError
+                        | NameAccessError
                         | DeadCode
+
+instance Show SemanticsError where
+    show (SemanticsError f o l sc ec e) = T.unpack f ++ ":"
+                                          ++ (show l) ++ ":"
+                                          ++ (show sc) ++ ":\n"
+                                          ++ take offset (repeat ' ') ++ "|\n"
+                                          ++ (show l) ++ " | " ++ T.unpack o
+                                          ++ take offset (repeat ' ') ++ "| "
+                                          ++ take sc (repeat ' ')
+                                          ++ take (ec - sc) (repeat '^') ++ "\n"
+                                          ++ show e
+        where offset = 1 + (length $ show l)
+
+instance Show SemanticsErrorType where
+    show (DuplicateDeclaration iden) = "cannot redeclare already declared identifier " ++ T.unpack iden
+    show (VoidVarDeclaration iden) = "cannot declare variable " ++ T.unpack iden ++ " as type void"
+    show (UndefinedIdentifier iden) = "cannot reference the undefined identifier " ++ T.unpack iden
+    show (TypeError t1 t2) = "expected type " ++ show' t1 ++ ", got type " ++ show' t2
+    show (CastError t1 t2) = "couldn't case type " ++ show' t1 ++ " to type " ++ show' t2
+    show (CallError iden n1 n2) = "function " ++ T.unpack iden ++ " expected " ++ show n1 ++ " arguments, got " ++ show n2
+    show AddressError = "can't take address of non-lvalue"
+    show AssignError = "can't assign to a non-lvalue"
+    show (ImproperIdentifier iden) = "improperly used identifier " ++ T.unpack iden
+    show (FieldAccessError iden1 iden2) = "struct type " ++ T.unpack iden1 ++ " doesn't contain a field named " ++ T.unpack iden2
+    show LValueAccessError = "cannot access a field of a non-lvalue"
+    show NameAccessError = "cannot access a field with an expression"
+    show DeadCode = "code is never reached"
+
+show' :: Type -> String
+show' Void = "void"
+show' Bool = "bool"
+show' U8 = "u8"
+show' U16 = "u16"
+show' U32 = "u32"
+show' U64 = "u64"
+show' I8 = "i8"
+show' I16 = "i16"
+show' I32 = "i32"
+show' I64 = "i64"
+show' F16 = "f16"
+show' F32 = "f32"
+show' F64 = "f64"
+show' (StructType t) = T.unpack t
